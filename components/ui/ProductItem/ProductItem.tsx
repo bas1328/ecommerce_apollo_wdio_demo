638d04929ca0682f8ee7c7124e965e82e4cdbf1d
Andrei Bodrov <bas1328@mail.ru>
2023-09-20 15:57:41 -0500

    feat: add e2e job

--- components/ui/ProductItem/ProductItem.tsx
+++ components/ui/ProductItem/ProductItem.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
+import styles from "./ProductItem.module.scss";
 interface ProductItemProps {
   name: string;
   image: string;
@@ -14,7 +15,11 @@ export default function ProductItem({ name, image, id }: ProductItemProps) {
     <>
       <Link href={`/product-list/${id}`}>
         <h1>Product Item</h1>
-        <Image src={image} alt={name} width={500} height={500} />
+        <div className={styles.wrapper}>
+          <Image className={styles.base} src="/tshirt.png" alt={name} width={600} height={600} />
+          <Image className={styles.printImage} src={image} alt={name} width={200} height={200} />
+        </div>
+
         <h2>{name}</h2>
       </Link>
       <button onClick={addToCartHandler}>Add to cart</button>
